3292990679ea0ac671ec8babe58e58528cff4688
Luik <t.t.luik@amsterdamumc.nl>
2026-05-26 16:28:51 +0200

    Add multiple file option

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ parser = argparse.ArgumentParser(description='Copy input file to output folder')
 parser.add_argument('--inputfile', required=True, help='input file')
 parser.add_argument('--outputfolder', required=True, help='output folder')
 parser.add_argument('--altoutputfolder', help='alternative output folder')
+parser.add_argument('--measurement_table', default=None, help='directory containing measurement/table files to pass through')
+parser.add_argument('--extra_array', default=None, help='path to numpy array file (.npy/.npz) to pass through')
+parser.add_argument('--config_file', default=None, help='path to config/parameter file to pass through')
+parser.add_argument('--custom_model', default=None, help='path to model weights or executable to pass through')
 parser.add_argument('--show_progress', action='store_true')
 parser.add_argument('--verbose', action='store_true')
 # Parameters
@@ -32,6 +36,10 @@ result = process(
     an_axis=args.an_axis,
     a_count=args.a_count,
     logfile=args.logfile,
+    measurement_table_dir=args.measurement_table,
+    extra_array=args.extra_array,
+    config_file=args.config_file,
+    custom_model=args.custom_model,
 )
 
 if result and result != '{}':
--- process.py
+++ process.py
@@ -3,10 +3,24 @@ import os.path
 import shutil
 
 
+def _copy_file_to_output(src, output_folder, label):
+    """Copy a single file to output_folder. Returns dest path or None."""
+    if not src:
+        return None
+    if os.path.isfile(src):
+        dest = os.path.join(output_folder, os.path.basename(src))
+        shutil.copy2(src, dest)
+        print(f'[{label}] copied {src} -> {dest}')
+        return dest
+    print(f'[{label}] WARNING: file not found: {src}')
+    return None
+
+
 def process(input_filename, output_folder, alt_output_folder=None,
             show_progress=False, verbose=False, max_attempts=1,
             pick_one='alpha', a_number=1.0, an_axis=0, a_count=10,
-            logfile='', **kwargs):
+            logfile='', measurement_table_dir=None,
+            extra_array=None, config_file=None, custom_model=None, **kwargs):
 
     print(f'[params] pick_one={pick_one!r} a_number={a_number} an_axis={an_axis} a_count={a_count}')
 
@@ -32,6 +46,34 @@ def process(input_filename, output_folder, alt_output_folder=None,
     if show_progress:
         print(message)
 
+    # Copy measurement table files to output and track them
+    copied_tables = []
+    if measurement_table_dir and os.path.isdir(measurement_table_dir):
+        for fname in sorted(os.listdir(measurement_table_dir)):
+            src = os.path.join(measurement_table_dir, fname)
+            if os.path.isfile(src):
+                dest = os.path.join(output_folder, fname)
+                shutil.copy2(src, dest)
+                copied_tables.append(dest)
+                print(f'[table] copied {src} -> {dest}')
+    elif measurement_table_dir:
+        print(f'[table] WARNING: directory not found: {measurement_table_dir}')
+    if copied_tables:
+        result['measurement_tables'] = copied_tables
+
+    # Copy single-file attachments to output
+    copied_extra_array = _copy_file_to_output(extra_array, output_folder, 'extra_array')
+    if copied_extra_array:
+        result['extra_array'] = copied_extra_array
+
+    copied_config_file = _copy_file_to_output(config_file, output_folder, 'config_file')
+    if copied_config_file:
+        result['config_file'] = copied_config_file
+
+    copied_custom_model = _copy_file_to_output(custom_model, output_folder, 'custom_model')
+    if copied_custom_model:
+        result['custom_model'] = copied_custom_model
+
     if logfile:
         os.makedirs(os.path.dirname(logfile), exist_ok=True) if os.path.dirname(logfile) else None
         with open(logfile, 'w') as f:
@@ -41,6 +83,14 @@ def process(input_filename, output_folder, alt_output_folder=None,
             f.write(f'a_count={a_count}\n')
             f.write(f'input={input_filename}\n')
             f.write(f'output={output_path}\n')
+            for i, t in enumerate(copied_tables):
+                f.write(f'measurement_table_{i}={t}\n')
+            if copied_extra_array:
+                f.write(f'extra_array={copied_extra_array}\n')
+            if copied_config_file:
+                f.write(f'config_file={copied_config_file}\n')
+            if copied_custom_model:
+                f.write(f'custom_model={copied_custom_model}\n')
         result['logfile'] = logfile
 
     return json.dumps([result])
